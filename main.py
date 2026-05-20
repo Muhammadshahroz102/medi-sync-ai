7537b8a6d35ae71c74a3422682ec942438bdd77a
muhammadshahroz102 <muhammadshahroz102@gmail.com>
2026-05-20 13:39:58 +0200

    changes to backend main

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # Your Next.js URL
+    allow_origins=["http://localhost:3000", "http://46.101.155.231:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
